42fbc7ea3b72815c194807f9459b4e68d0c1fade
huynhhiep3006 <huynhhiep3006@gmail.com>
2025-07-22 15:30:43 +0700

    Force rebuild after clearing .next

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -1,22 +1,24 @@
 'use client';
 
-//import { usePathname } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import { ReactNode } from 'react';
 import AmazonPopup from '@/components/AmazonPopup';
 import RelatedPostsPopup from '@/components/RelatedPostsPopup';
 
 export default function LayoutWrapper({ children }: { children: ReactNode }) {
-  //const pathname = usePathname();
-//   const isPostPage =
-//     /^\/[a-z0-9-]+$/.test(pathname) &&
-//     !['/gear', '/about', '/guides', '/info-national-park', '/', '/upload', '/listhtml'].includes(pathname) &&
-//     !pathname.startsWith('/topic');
+  const pathname = usePathname();
+  const isPostPage =
+    /^\/[a-z0-9-]+$/.test(pathname) ;
 
   return (
     <>
       {children}
-       <AmazonPopup />
+      {isPostPage && (
+        <>
+          <AmazonPopup />
           <RelatedPostsPopup />
+        </>
+      )}
     </>
   );
 }
