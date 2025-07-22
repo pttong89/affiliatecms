0fa087d76cb021103d113508784040114f5ab68f
huynhhiep3006 <huynhhiep3006@gmail.com>
2025-07-22 15:38:07 +0700

    Force rebuild after clearing .next

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -6,19 +6,24 @@ import AmazonPopup from '@/components/AmazonPopup';
 import RelatedPostsPopup from '@/components/RelatedPostsPopup';
 
 export default function LayoutWrapper({ children }: { children: ReactNode }) {
-  const pathname = usePathname();
-  const isPostPage =
-    /^\/[a-z0-9-]+$/.test(pathname) ;
+    const pathname = usePathname();
+    const safePath = pathname || '';
+    const excludedPaths = ['/gear', '/about', '/guides', '/info-national-park', '/', '/upload', '/listhtml'];
 
-  return (
-    <>
-      {children}
-      {isPostPage && (
+    const isPostPage =
+        /^[a-z0-9-]+$/.test(safePath.replace(/^\//, '')) &&  // kiểm tra path dạng slug
+        !excludedPaths.includes(safePath) &&
+        !safePath.startsWith('/topic');
+
+    return (
         <>
-          <AmazonPopup />
-          <RelatedPostsPopup />
+            {children}
+            {isPostPage && (
+                <>
+                    <AmazonPopup />
+                    <RelatedPostsPopup />
+                </>
+            )}
         </>
-      )}
-    </>
-  );
+    );
 }
